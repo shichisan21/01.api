387942c87105bfd4b69dcd58eebbb5ba309b2be1
shichisan21 <photograph.by@gmail.com>
2023-06-24 21:24:17 +0900

    チェック用

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 from pydantic import BaseModel
 import openai
 import os
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 # cors_origin = os.getenv("CORS_ORIGIN")
 
 app = FastAPI()
