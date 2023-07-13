8cb83f5b48352ed997ff3fe46a614971b1c6c626
shichisan21 <photograph.by@gmail.com>
2023-07-13 21:06:30 +0900

    Dynamo接続

--- routes/talk.py
+++ routes/talk.py
@@ -5,10 +5,22 @@ router = APIRouter()
 
 
 def create_dynamodb_client():
-    dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
+    dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
     return dynamodb
 
 
-@router.get("/talk")
-def Talk():
-    return {"Hello": "World"}
+def get_item(dynamodb, table_name, id):
+    table = dynamodb.Table(table_name)
+    response = table.get_item(
+        Key={
+            "Id": id
+        }
+    )
+    return response.get("Item", None)
+
+
+@router.get("/get_item/{id}")
+def read_item(id: int):
+    dynamodb = create_dynamodb_client()
+    item = get_item(dynamodb, "talk-app-table", id)
+    return item
