5012d0e24753a106672100504376a7f5785e8a8d
shichisan21 <photograph.by@gmail.com>
2023-07-14 23:33:23 +0900

    POST

--- routes/talk.py
+++ routes/talk.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 import boto3
 
 router = APIRouter()
+
+
+class Item(BaseModel):
+    id: int
+    name: str
 
 
 def create_dynamodb_client():
@@ -19,8 +25,25 @@ def get_item(dynamodb, table_name, id):
     return response.get("Item", None)
 
 
+def put_item(dynamodb, table_name, item):
+    table = dynamodb.Table(table_name)
+    response = table.put_item(
+        Item={"Id": item.id, "name": item.name}
+    )
+    return response
+
+
 @router.get("/get_item/{id}")
 def read_item(id: int):
     dynamodb = create_dynamodb_client()
     item = get_item(dynamodb, "talk-app-table", id)
     return item
+
+
+@router.post("/put_item/")
+def create_item(item: Item):
+    dynamodb = create_dynamodb_client()
+    response = put_item(dynamodb, "talk-app-table", item)
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        raise HTTPException(status_code=400, detail="Item could not be stored")
+    return response
