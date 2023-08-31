2369cb712460d2721d296c5bdd3ab2da7f349fd2
shichisan21 <photograph.by@gmail.com>
2023-08-31 21:21:30 +0900

    時刻を返すAPIを作成

--- routes/calc.py
+++ routes/calc.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
+from datetime import datetime
+import pytz
 
 router = APIRouter()
 
 
-@router.get("/calc")
-def Calc():
-    return "a"+"3"
+@router.get("/current_time")
+def get_current_time():
+    local_time = datetime.now().astimezone(pytz.timezone('Asia/Tokyo'))  # 例として日本時間
+    utc_time = datetime.utcnow().replace(tzinfo=pytz.UTC)
+
+    return {
+        "local_time": local_time.isoformat(),
+        "utc_time": utc_time.isoformat()
+    }
